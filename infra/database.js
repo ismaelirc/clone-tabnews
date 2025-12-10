f8e1bf7286df6be7f99afee6d1feb86f699698f6
Ismael Costa <ismaelirc@gmail.com>
2025-12-10 12:09:05 -0300

    configurando SSL

--- infra/database.js
+++ infra/database.js
@@ -5,8 +5,11 @@ async function query(queryObject) {
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
-    database: process.env.DB,
+    database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: {
+      rejectUnauthorized: false,
+    },
   });
 
   try {
