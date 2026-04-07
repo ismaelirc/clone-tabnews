c4622c478bfee35ebab6a2f908688636a77259ec
Ismael Costa <ismaelirc@gmail.com>
2026-04-07 12:05:43 -0300

    Ajustando testes

--- jest.config.js
+++ jest.config.js
@@ -12,7 +12,7 @@ const createJestConfig = nextJest({
 
 const jestConfig = createJestConfig({
   moduleDirectories: ["node_modules", "<rootDir>"],
-  testeTimeout: 60000,
+  testTimeout: 60000,
 });
 
 module.exports = jestConfig;
