d116481f72d1eaa12866e10097deb88ada797237
Ismael Costa <ismaelirc@gmail.com>
2026-04-17 13:39:01 -0300

    ajustes

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -14,7 +14,7 @@ test("GET to /api/v1/status should return 200", async () => {
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
   // versao do pg
-  //expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
   // quantidade conexao maxima
   expect(
     responseBody.dependencies.database.max_connections,
