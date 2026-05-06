1a60502c368e6727323584aa16cd2a0d996399ef
Ismael Costa <ismaelirc@gmail.com>
2026-05-06 11:31:28 -0300

    chore: add --verbose to test:watch

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -10,14 +10,18 @@ async function cleanDatabase() {
   await database.query("drop schema public cascade; create schema public;");
 }
 
-test("GET to /api/v1/migrations should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations");
+describe("GET /api/v1/migrations should return 200", () => {
+  describe("Anonymus user", () => {
+    test("Runing pendind migrations", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/migrations");
 
-  expect(response.status).toBe(200);
+      expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
+      const responseBody = await response.json();
 
-  expect(Array.isArray(responseBody)).toBe(true);
+      expect(Array.isArray(responseBody)).toBe(true);
 
-  expect(responseBody.length).toBeGreaterThan(0);
+      expect(responseBody.length).toBeGreaterThan(0);
+    });
+  });
 });
